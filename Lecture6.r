d2486cfb5c59c681609e69e057ac6c8bd918dbb4
Yi Lian <yi.lian@mail.mcgill.ca>
2019-10-10 23:41:03 -0400

    Lecture6 updated

--- Lecture6.r
+++ Lecture6.r
@@ -8,43 +8,44 @@ plot(x = iris$Sepal.Length, y = iris$Sepal.Width,
      main = "Sepal Width vs. Sepal Length",    # Title
      sub = "Iris",                             # Sub title
      xlim = range(iris$Sepal.Length),          # limits of x-axis
-#      ylim =,                                   # Limits of y-axis
+     ylim = c(1, 10),                                   # Limits of y-axis
      xlab = "Sepal Length",                    # Label of x-axis
-     ylab = "Sepal Length",                    # Label of y-axis
-#      log = "x"                                 # Axis to be set on log scale
+     ylab = "Sepal Width",                    # Label of y-axis
+     log = "xy"                                 # Axis to be set on log scale
 )            
 # x and y are arguments - these two are sufficient for a plot
 # In this plot, I specified some most basic graphical parameters.
 
 # Some parameters related to text and plot size.
 plot(iris$Sepal.Length,
-     type = "o",    # Type of plot, default is points.
-     pch = "*",     # plotting symbols, 1~25 plus anything you want.
-     cex = 3,       # Plotting text and symbol size
-     cex.axis = 3,  # Axis annotation size
+     type = "p",    # Type of plot, default is points.
+     pch = "µ",     # plotting symbols, 1~25 plus anything you want.
+     cex = 2,       # Plotting text and symbol size
+     cex.axis = 2,  # Axis annotation size
      main = "Sepal Length", 
-     cex.main = 3,  # Title text size
-     cex.lab = 1.5, # Axis label size
-     lwd = 5,       # Line width
-     lty = 3
+     cex.main = 2,  # Title text size
+     cex.lab = 1,   # Axis label size
+     lwd = 1,       # Line width
+     lty = 4
 )
 
 table(iris$Species)
 
 # My second pie chart......
 pie(x = table(iris$Species),
-    col =,
+#     col = 1:3,
     clockwise = T,
-    main = "Species of Iris",
+    main = "Species of Iris",  
 )
 
 # Some parameters related to colors
 par(bg = "lightyellow") # par() sets graphical parameters before plots.
+
 hist(iris$Sepal.Length,
      freq = F,             # count or proportion
 #      breaks = 15,
-     breaks = seq(4,8,0.5),
-     xlim = c(0, 10),
+     breaks = seq(from = 4,to = 8,by = 0.5),
+     xlim = range(iris$Sepal.Length),
      main = "Histogram of Sepal Length",
      sub = "Iris",
      xlab = "Sepal Length",
@@ -54,8 +55,8 @@ hist(iris$Sepal.Length,
      col.sub = 4,           # multiple ways to specify color.
      col = "darkgreen",
      border = "blue",       # Color of border of the bars
-     density = 8,           # density of shading lines
-     angle = 65             # angle of shading lines, in degrees.
+     density = 2,           # density of shading lines
+     angle = 15             # angle of shading lines, in degrees.
 )
 # border, density and angle are parameters specific to hist().
 # Mostly showed parameters related to colors.
@@ -73,6 +74,7 @@ boxplot(iris$Sepal.Length, iris$Sepal.Width, iris$Petal.Length, iris$Petal.Width
 
 fx1 <- function(x){x^2-10}
 fx2 <- function(x){x^3}
+
 curve(fx1,
       xlim = c(-10, 10), ylim = c(-10, 10),
       col = 2, lty = 1)
@@ -80,10 +82,12 @@ curve(fx2, add = TRUE,   # add is an parameter in curve()
       col = 3, lty = 2)  # TRUE -> plot on the existing plot
 
 x <- seq(from = -10, to = 2, by = 0.25)
-y <- exp(x)
-points(x=x, y=y, pch = ">")  # Add these points to the existing plot
+y1 <- exp(x[1:24])
+y2 <- exp(x[25:49])
+points(x=x[1:24], y=y1, pch = ">")  # Add these points to the existing plot
+lines(x=x[25:49], y=y2)
 # Add the smooth line containing these points to the existing plot
-# lines(x=x, y=y)            
+# lines(x=x, y=y)     
 
 abline(h = 5, lty = 3)      # h -> horizontal line at y = 5 
 abline(v = -8, lty = "dotdash")     # v => vertical line at x = -8
@@ -91,7 +95,7 @@ abline(v = -8, lty = "dotdash")     # v => vertical line at x = -8
 abline(a = 0, b = 1/2)      # y = a + bx
 
 # legend
-legend(x = 5, y = -7,       # Can also be "top", "bottomright", ...
+legend("topright",       # Can also be "top", "bottomright", ...
        c(expression(paste(x^2-10)), expression(paste(x^3))),
        col = c(2,3),      # Usually corresponds to the plot
        lty = c(1,2),
@@ -105,19 +109,22 @@ plot(x = xx, y = yy, type = "l", main = "PDF of Normal(0, 2)",
      xlab = "x", ylab = "Density"
 )
 # axis() allows us to customize axes.
-axis(1, at = seq(from = -10, to = 10, by = 2))
+axis(1, at = seq(from = -10, to = 10, by = 4))
 axis(2, at = seq(from = 0, to = 1, by = 0.02))
 grid()   # add grid lines.
 # Explore the parameters allowed in grid()
 
-par(mfrow = c(2, 2)) # 2 x 2 = 4 plots on the same page
+par(mfrow = c(2,2), mar = c(3, 2, 1, 1) + 0.1) # 2 x 2 = 4 plots on the same page, mar allows us to change margin
 plot(lm(Sepal.Length~Petal.Length, data = iris))
 # lm() for linear regression - EPIB 621 material
 # Plot your linear regression object will give 4 diagnostic plots.
 
+matrix(c(1,1,2,3), 2, 2, byrow = TRUE)
+
 nf <- layout(matrix(c(1,1,2,3), 2, 2, byrow = TRUE))
+par(mar = c(3, 2, 1, 1) + 0.1)
 # layout.show(nf)  # Shows the partition of the plotting area
-plot(x=x, y=y, type = "l")
+plot(x=x, y=c(y1, y2), type = "l")
 plot(x=xx, y=yy, type = "l")
 boxplot(iris$Sepal.Length, iris$Petal.Length)
 
@@ -131,6 +138,22 @@ grid()
 
 dev.off()
 
-mu <- 0:4
-sigma <- 1:5
-bg.color <- c("", "", "", "", "") # Colors of your choice
+# mu <- 0:4
+# sigma <- 1:5
+bg.color <- c("grey90", "grey80", "grey70", "grey60", "grey50") # Colors of your choice
+
+x <- seq(from = -15, to = 20, by = 0.1)
+for (i in 1:5) {
+    y <- dnorm(x, mean = i-1, sd = i)
+    
+    pdf(file = paste("Normal_Density_", i, ".pdf", sep = ""))
+    par(bg = bg.color[i])
+    
+    plot(x = x, y = y, type = "l", lty = i, xlim = c(-15, 20),
+         main = paste("PDF of Normal(", i-1, ",", i, ")", sep = ""))
+    abline(v = i-1, col = "maroon4")
+    grid()
+    dev.off()
+}
+
+
